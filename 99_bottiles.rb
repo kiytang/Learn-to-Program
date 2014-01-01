4a278961c1f6205e6a6b4be11bfd3f3e77d4b5cd
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-01 17:23:46 +0000

    added 99_bottles

---
+++ 99_bottiles.rb
@@ -0,0 +1,26 @@
+# ”write a progrma that prints out the lyrics to “99 Bottles of Beer on the Wall
+
+bottles = 99
+
+# used > 2 aso that the plural bottles conforms correctly
+while bottles > 2
+	puts "#{Bottles} bottles of beer on the wall, "
+	puts "#{bottles} bottles of beer,"
+	puts "You take one down and pass it around," 
+	bottles -= 1
+	puts "#{bottles} bottles of beer on the wall!"
+	puts
+end
+
+puts "2 bottles of beer on the wall,"
+puts "2 bottles of beer,"
+puts "Take one down, pass it around,"
+puts "1 more bottle of beer on the wall!"
+
+puts
+
+puts "1 bottle of beer on the wall,"
+puts "Only 1 bottle of beer,"
+puts "Take it down, pass it around,"
+puts "No more bottles of beer on the wall!"
+puts "Need to buy another 99 bottles!!"
