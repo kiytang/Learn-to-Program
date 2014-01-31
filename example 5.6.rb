96dcbc5b4285d32e1ba41a7d36fb3656ead04781
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-31 18:31:58 +0000

    completed ex5.6

---
+++ example 5.6.rb
@@ -0,0 +1,14 @@
+puts 'Ciao, nice to meet you'
+puts 'What\'s your first name?'
+f_name = gets.chomp
+puts 'What\'s your middle name?'
+m_name = gets.chomp
+puts 'What\'s your last name?'
+l_name = gets.chomp
+
+full_name = f_name + ' ' + m_name + ' ' + l_name
+
+puts 'Pleased to meet you, ' + full_name + '!'
+# puts 'Please to meet you, ' + f_name + + m_name + + l_name + ':)'
+# puts 'Your name is ' + name + '? What a lovely name!'
+# puts 'Pleased to meet you, ' + name + '. :)' 
