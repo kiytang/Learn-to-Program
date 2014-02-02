da17251de06cb6318b595ba5b26b51a5f3731308
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-02 16:03:18 +0000

    completed ch8,1

---
+++ ch8.1_method.rb
@@ -0,0 +1,11 @@
+languages = ['English', 'Japanese', 'Ruby']
+
+# lang is the variable that each will use to point to the objects
+# in the array. 
+languages.each do |lang|
+	puts 'I love ' + lang + '!'
+	puts 'Don\'t you?'
+end
+
+puts 'And let\'s hear it for Java!'
+puts '<crickets chirp in the distance>'
