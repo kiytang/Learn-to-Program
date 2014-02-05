ac45b4e76f9cc3fc9c2f11f8856ccfcdb8d02c33
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-05 21:21:21 +0000

    completed ch9.4 ask return

---
+++ ch9.4_ask_retun_version.rb
@@ -0,0 +1,45 @@
+# Improved ask method. That ask method I showed you was OK, but I
+# bet you could do better. Try to clean it up by removing the answer
+# variable. You’ll have to use return to exit from the loop. (Well, it will
+# get you out of the whole method, but it will get you out of the loop
+# How do you like the resulting method? I usually
+# try to avoid using return (a personal preference), but I might make
+# an exception here.
+
+def ask question       #ask is method, question is th parameter
+
+	puts question
+	reply = gets.chomp.downcase
+
+	# if reply != 'yes' && reply != 'no'
+	while reply != 'yes' && reply != 'no'
+		puts 'Please answer "yes" or "no".'
+		puts question
+		reply = gets.chomp.downcase
+		# goto 14
+	end
+
+		if reply == 'yes'
+			return true
+		else
+			return false
+		end
+end
+
+puts 'Hello, and thank you for ...'
+puts
+
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING'
+puts 'Thank you for ...'
+puts
+puts wets_bed
